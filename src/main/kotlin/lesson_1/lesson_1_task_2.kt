8f7ae6c86075c2550744e26ae9c9b514aac9f93e
NurGuru <nurchibald@mail.ru>
2023-03-14 18:48:23 +0300

    KS-1-2

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrder = 75
+    val thanksMessage = "Thank you for choosing us"
+    var employeesNumber = 2000
+
+    println(numberOfOrder)
+    println(thanksMessage)
+//    println(employeesNumber)
+    employeesNumber = 1999
+    println(employeesNumber)
+}
