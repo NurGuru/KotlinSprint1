89585709a590f042370ca1ba1d46dfa52e4f3f3e
NurGuru <nurchibald@mail.ru>
2023-03-14 19:01:31 +0300

    KS-1-3

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,15 @@
+package lesson_1
+fun main() {
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println("""
+        $year
+        $hour
+        $minute
+    """.trimIndent())
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
+}
