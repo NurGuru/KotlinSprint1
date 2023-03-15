b34c48ec7d1b7f5eec6230c64a9135fdcf7b000e
NurGuru <nurchibald@mail.ru>
2023-03-15 21:44:58 +0300

    KS-3-2 исправил все вроде)

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -4,14 +4,12 @@ fun main() {
     val startHour = 9
     val startMinute = 39
     val timeInRoad = 457
-    val arrivalHour = startHour + (timeInRoad / 60)+(startMinute + timeInRoad % 60)/60
-    val arrivalMinute = (startMinute + timeInRoad % 60)%60
+    val arrivalHour = startHour + (timeInRoad / 60) + (startMinute + timeInRoad % 60) / 60
+    val arrivalMinute = (startMinute + timeInRoad % 60) % 60
     //что-то мне подсказывает, что есть поэелегантней решение, но его его увы не вижу).
     // я и над этим элементарным моментом долго думал)
 
     println("$arrivalHour:$arrivalMinute")
-
-
 
 
 //    println("$arrivalHour:$arrivalMinute")
