b40c14f4e8e9abb141f8ea0b2448232b375153c5
NurGuru <nurchibald@mail.ru>
2023-03-15 15:41:52 +0300

    KS-2-5

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -1,2 +1,13 @@
 package lesson_2
 
+import kotlin.math.pow
+
+
+fun main() {
+    val firstContribution = 70000
+    val percent = 16.7
+    val timePeriod = 20
+    val result: Double = (1 + percent / 100).pow(timePeriod) * firstContribution
+//я что-то не смог разобраться, как сделать три знака после запятой
+    println(result)
+}
