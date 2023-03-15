b58bfc52d9957e0ccb081dbd758effb6decd3285
NurGuru <nurchibald@mail.ru>
2023-03-15 14:16:35 +0300

    KS-2-3

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -1,2 +1,15 @@
 package lesson_2
 
+fun main() {
+    val startlHour = 9
+    val startMinute = 39
+    val timeInRoad = 457
+    var arrivalHour =startlHour +(timeInRoad/60)
+    var arrivalMinute =startMinute +(timeInRoad%60)
+    if (arrivalMinute>60) {
+        arrivalHour = arrivalHour+1
+        arrivalMinute = arrivalMinute - 60
+    }
+//я не знал, как по другому сделать, чтоб минуты в час первести))
+    println("$arrivalHour:$arrivalMinute")
+}
