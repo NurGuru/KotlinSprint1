2858aaf578ca7624c5ab8314b4f00c3e774ca325
NurGuru <nurchibald@mail.ru>
2023-03-16 09:00:10 +0300

    KS-3-3

--- src/main/kotlin/lesson_3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -1,2 +1,20 @@
 package lesson_3
 
+fun main() {
+    var number = 7
+
+    println(
+        """
+        ${number * 1} 
+        ${number * 2} 
+        ${number * 3} 
+        ${number * 4} 
+        ${number * 5} 
+        ${number * 6} 
+        ${number * 7} 
+        ${number * 8} 
+        ${number * 9} 
+        ${number * 10} 
+    """.trimIndent()
+    )
+}
