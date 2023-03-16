e4e48c9a6dc77b0c6f8ee3f6e5217c26e587af09
NurGuru <nurchibald@mail.ru>
2023-03-16 09:24:52 +0300

    KS-3-5

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -1,2 +1,12 @@
 package lesson_3
 
+fun main() {
+    var move = "D2-D4;0"
+    var from = "${move[0]}${move[1]}"
+    var to = "${move[3]}${move[4]}"
+    var id = move[6]
+
+    println(from)
+    println(to)
+    println(id)
+}
