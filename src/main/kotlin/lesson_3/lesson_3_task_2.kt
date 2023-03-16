bfbfe46adc272e473f89f56fc62f3ca8bd46fed6
NurGuru <nurchibald@mail.ru>
2023-03-16 08:53:03 +0300

    KS-3-2

--- src/main/kotlin/lesson_3/lesson_3_task_2.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -1,13 +1,2 @@
 package lesson_3
 
-fun main() {
-    var name = "Татьяна"
-    var surname = "Андреева"
-    var patronymic = "Сергеевна"
-    var age = "20"
-
-    println("[$surname $name $patronymic,$age]")
-    surname = "Сидорова"
-    age = "22"
-    println("[$surname $name $patronymic,$age]")
-}
