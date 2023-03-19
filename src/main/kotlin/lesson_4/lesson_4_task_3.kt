9b34b00d39813bdb87dd5231dd9962914ef50dec
NurGuru <nurchibald@mail.ru>
2023-03-19 14:43:52 +0300

    KS-4-3

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,2 +1,11 @@
 package lesson_4
 
+fun main() {
+    val weather = "солнечная погода"
+    val tent = "тент раскрыт"
+    val humidity = "20%"
+    val season = "winter"
+    val suitableDay = weather == "солнечная погода" && tent == "тент раскрыт" && humidity == "20%" && season != "winter"
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $suitableDay")
+}
