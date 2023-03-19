b80f299ce41e62dc6ae3d0221bf5be23ef95b96b
NurGuru <nurchibald@mail.ru>
2023-03-19 13:54:18 +0300

    KS-2-5

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -1,2 +1,19 @@
 package lesson_4
 
+fun main() {
+
+    val reservedTableForToday = 13
+    val reservedTableForTomorrow = 9
+
+    val freeTablesToday = reservedTableForToday < MAX_TABLE
+    val freeTablesTomorrow = reservedTableForTomorrow < MAX_TABLE
+
+
+    println(
+        "Доступность столиков на сегодня:$freeTablesToday " +
+                "Доступность столиков на завтра:$freeTablesTomorrow"
+    )
+    //что-то не смог разобраться как их на разные строки поставить, подскажи пожалуйста
+}
+
+const val MAX_TABLE = 13
