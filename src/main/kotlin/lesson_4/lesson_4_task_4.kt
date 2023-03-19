c7d56f37f677a18132714754d9a67011af988b19
NurGuru <nurchibald@mail.ru>
2023-03-19 21:09:47 +0300

    KS-4-4

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -1,2 +1,14 @@
 package lesson_4
 
+fun main() {
+    var day = 5
+    val arms = day % 2 !== 0
+    val legs = day % 2 == 0
+    val back = day % 2 == 0
+    val press = day % 2 !== 0
+
+    println("Упражнения для рук: $arms\nУпражнения для ног: $legs\nУпражнения для спины: $back\nУпражнения для пресса: $press")
+    println()
+    day = 6
+    println("Упражнения для рук: $arms\nУпражнения для ног: $legs\nУпражнения для спины: $back\nУпражнения для пресса: $press")
+}
