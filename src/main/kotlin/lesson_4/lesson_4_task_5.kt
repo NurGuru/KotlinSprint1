d7cd20f9a7612a5bdf984cbab150657d42b5ffd7
NurGuru <nurchibald@mail.ru>
2023-03-20 10:40:33 +0300

    KS-4-5

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -1,2 +1,19 @@
 package lesson_4
 
+fun main() {
+    val breaks = readln().toBoolean()
+    val teammates = readln().toInt()
+    val foodBoxes = readln().toInt()
+    val goodWeather = readln().toBoolean()
+    val mayGo = breaks == false && teammates >= 55 && teammates < 70 && foodBoxes> MIN_FOOD_BOXES
+            && goodWeather==true || false
+            ||breaks == true && teammates >= 55 && teammates == 70 && foodBoxes> MIN_FOOD_BOXES
+            && goodWeather==true
+
+    println("Можно отправляться? - $mayGo")
+}
+
+
+const val MIN_TEAMMATES = 55
+const val MAX_TEAMMATES = 70
+const val MIN_FOOD_BOXES = 50
