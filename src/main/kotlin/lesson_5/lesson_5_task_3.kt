796f058815c7a4123d79374a6a115b22f6ddc859
NurGuru <nurchibald@mail.ru>
2023-03-20 12:21:21 +0300

    KS-5-3

--- src/main/kotlin/lesson_5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -1,2 +1,19 @@
 package lesson_5
 
+fun main() {
+
+    val winNumber1 = 5
+    val winNumber2 = 33
+    println("Введите два числа от 1 до 100")
+    val usersTry1 = readln().toInt()
+    val usersTry2 = readln().toInt()
+
+    if (usersTry1 == winNumber1 && usersTry2 == winNumber2 || usersTry1 == winNumber2 && usersTry2 == winNumber1) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (usersTry1 == winNumber1 || usersTry1 == winNumber2 || usersTry2 == winNumber1 || usersTry2 == winNumber2) {
+        println("Вы выиграли утешительный приз!")
+    } else {
+        println("Неудача! Крутите барабан!")
+
+    }
+}
