6422e54a71f737b13fc9f133ef9c1d2eaf447a38
NurGuru <nurchibald@mail.ru>
2023-03-21 10:02:03 +0300

    KS-6-1

--- src/main/kotlin/lesson_6/lesson_6_task_1.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_1.kt
@@ -1,1 +1,22 @@
 package lesson_6
+
+fun main() {
+    println("Создайте логин")
+    val regLogin = readln()
+    println("Создайте пароль")
+    val regPassword = readln()
+
+    println("Введите логин")
+    val login = readln()
+    println("Введите пароль")
+    var password = readln()
+    if (password == regPassword) {
+        println("Авторизация прошла успешно.")
+    } else {
+        println("Введен неверный пароль")
+        while (password != regPassword) {
+            println("Введите пароль заново")
+            password = readln()
+        }
+    }
+}
