a3e205d285358ddd48a63fb4f724fddb21135cf3
NurGuru <nurchibald@mail.ru>
2023-03-22 00:27:31 +0300

    KS-6-4 вроде так)

--- src/main/kotlin/lesson_6/lesson_6_task_4.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_4.kt
@@ -3,17 +3,17 @@ package lesson_6
 fun main() {
     val winNumber = (1..9).random()
     var shot = 5
-    val gess = winNumber
 
-    while (shot != gess && shot > 0) {
+    println("Попробуй угадать число!")
+    while (shot > 0) {
         shot--
-        readln().toInt()
+        val tryNumber = readln().toInt()
+        if (tryNumber != winNumber) {
+            println("Попробуй еще раз!")
+        }
+        if (tryNumber == winNumber) {
+            println("Это была великолепная игра")
+        }
     }
-    if (shot == gess) {
-        println("Это была великолепная игра")
-    } else {
-        println("Было загадано число $gess")
-    }
-
-
+    println("Было загадано число $winNumber")
 }
