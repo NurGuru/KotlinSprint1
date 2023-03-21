31a2dd5f8378f6753ec08b9b592cc6267645e452
NurGuru <nurchibald@mail.ru>
2023-03-21 11:46:02 +0300

    KS-6-4

--- src/main/kotlin/lesson_6/lesson_6_task_4.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_4.kt
@@ -1,1 +1,19 @@
 package lesson_6
+
+fun main() {
+    val winNumber = (1..9).random()
+    var shot = 5
+    val gess = winNumber
+
+    while (shot != gess && shot > 0) {
+        shot--
+        readln().toInt()
+    }
+    if (shot == gess) {
+        println("Это была великолепная игра")
+    } else {
+        println("Было загадано число $gess")
+    }
+
+
+}
