50e852d85b76a8801093a3bf06c594f38f60622e
NurGuru <nurchibald@mail.ru>
2023-03-22 10:20:58 +0300

    KS-7-2

--- src/main/kotlin/lesson_7/lesson_7_task_2.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_2.kt
@@ -1,2 +1,16 @@
 package lesson_7
 
+fun main() {
+    var code = (1000..9999).random()
+    println("Ваш код авторизации $code")
+    println("Введите код авторизации")
+    var codeEntered = readln().toInt()
+
+    while (code != codeEntered) {
+        code = (1000..9999).random()
+        println("Ваш новый код авторизации $code")
+        println("Введите корректный код авторизации")
+        codeEntered = readln().toInt()
+    }
+        println("Добро пожаловать!")
+    }
