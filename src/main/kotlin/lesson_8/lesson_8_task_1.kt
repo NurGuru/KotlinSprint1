a2ad317675f0ddedffc14243e22d7656b9234a07
NurGuru <nurchibald@mail.ru>
2023-03-23 08:34:21 +0300

    KS-8-1

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -1,2 +1,11 @@
 package lesson_8
 
+fun main() {
+    val arrayOfDays = intArrayOf(14, 34, 44, 23, 22, 12, 34)
+    var viewsInAWeek = 0
+
+    for (i in arrayOfDays) {
+        viewsInAWeek += i
+    }
+    println(viewsInAWeek)
+}
