9cbfed56c81f721fa6213ad1466e1dbe8b825a8d
NurGuru <nurchibald@mail.ru>
2023-03-23 23:48:30 +0300

    KS-7-1 исправил

--- src/main/kotlin/lesson_7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -1,11 +1,11 @@
 package lesson_7
 
 fun main() {
-    val symbol1 = ('a'..'z').random().toString()
+    val symbol1 = ('a'..'z').random()
     val symbol2 = (1..9).random()
-    val symbol3 = ('a'..'z').random().toString()
+    val symbol3 = ('a'..'z').random()
     val symbol4 = (1..9).random()
-    val symbol5 = ('a'..'z').random().toString()
+    val symbol5 = ('a'..'z').random()
     val symbol6 = (1..9).random()
-    val password = println(symbol1+symbol2+symbol3+symbol4+symbol5+symbol6)
+    val password = println("$symbol1$symbol2$symbol3$symbol4$symbol5$symbol6")
 }
