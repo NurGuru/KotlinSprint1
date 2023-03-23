46f82eefb453554bbcd065f136a0426c24e812df
NurGuru <nurchibald@mail.ru>
2023-03-23 07:50:25 +0300

    KS-7-5 что-то я не вывожу эту задачу, можно подсказать ход какой дальше?)

--- src/main/kotlin/lesson_7/lesson_7_task_5.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_5.kt
@@ -1,2 +1,14 @@
 package lesson_7
 
+fun main() {
+
+    val passwordLength = readln().toInt()
+    val number = (1..9).random().toString()
+    val letter = ('a'..'z').random().toString()
+    val letterUp = ('A'..'Z').random().toString()
+
+    for (i in passwordLength downTo 0) {
+
+
+    }
+}
