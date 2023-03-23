29cf50b25d53516a13c8bd43e76a84503345cac9
NurGuru <nurchibald@mail.ru>
2023-03-23 10:52:40 +0300

    KS-8-4

--- src/main/kotlin/lesson_8/lesson_8_task_4.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_4.kt
@@ -1,2 +1,25 @@
 package lesson_8
 
+fun main() {
+    val arrayOfIngredients = arrayOf("яйцо", "грибы", "сметана", "перец", "соль")
+
+    for (i in arrayOfIngredients) {
+        println("Ингредиент №${arrayOfIngredients.indexOf(i) + 1} - $i")
+    }
+    println()
+    println("Какой ингредиент заменить?")
+    val ingredientForChange = readln()
+    val numberOfIngredientForChange = arrayOfIngredients.indexOf(ingredientForChange)
+    if (numberOfIngredientForChange == -1) {
+        println("Такого ингредиента нет в списке!")
+
+    } else {
+        println("Какой ингредиент добавить?")
+        val newIngredient = readln()
+        arrayOfIngredients[numberOfIngredientForChange] = newIngredient
+        println("Готово! Вы сохранили следующий список")
+        for (i in arrayOfIngredients) {
+            println("Ингредиент №${arrayOfIngredients.indexOf(i) + 1} - $i")
+        }
+    }
+}
