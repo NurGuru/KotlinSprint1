968e9feaf6a335df62803b842f1cf5d35a3019db
NurGuru <nurchibald@mail.ru>
2023-03-23 06:57:27 +0300

    KS-7-2 исправил

--- src/main/kotlin/lesson_7/lesson_7_task_2.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_2.kt
@@ -1,16 +1,13 @@
 package lesson_7
 
 fun main() {
-    var code = (1000..9999).random()
-    println("Ваш код авторизации $code")
-    println("Введите код авторизации")
-    var codeEntered = readln().toInt()
+    do {
 
-    while (code != codeEntered) {
-        code = (1000..9999).random()
-        println("Ваш новый код авторизации $code")
-        println("Введите корректный код авторизации")
-        codeEntered = readln().toInt()
-    }
-        println("Добро пожаловать!")
-    }
+        val code = (1000..9999).random()
+        println("Ваш код авторизации $code")
+        println("Введите код авторизации")
+        val codeEntered = readln().toInt()
+    } while (code != codeEntered)
+
+    println("Добро пожаловать!")
+}
