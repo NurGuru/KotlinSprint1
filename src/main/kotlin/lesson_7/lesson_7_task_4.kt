5e33b0b73009cbe47408f3b0cfb63bc095d797a2
NurGuru <nurchibald@mail.ru>
2023-03-23 07:09:36 +0300

    KS-7-4

--- src/main/kotlin/lesson_7/lesson_7_task_4.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_4.kt
@@ -1,2 +1,14 @@
 package lesson_7
 
+fun main() {
+    println("Сколько секунд засечь?")
+    val seconds = readln().toInt()
+
+    for (i in seconds downTo 1) {
+        println("Осталось $i секунд")
+        Thread.sleep(1000)
+    }
+    println("Время вышло")
+
+
+}
