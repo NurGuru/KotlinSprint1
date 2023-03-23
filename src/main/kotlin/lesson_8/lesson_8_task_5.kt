d863bf999d6b31fbe9debc58e180bc9af1d2d0c9
NurGuru <nurchibald@mail.ru>
2023-03-23 12:23:07 +0300

    KS-8-5

--- src/main/kotlin/lesson_8/lesson_8_task_5.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_5.kt
@@ -1,2 +1,16 @@
 package lesson_8
 
+fun main() {
+    println("Введите количество ингредиентов")
+    val numberOfIngredients = readln().toInt()
+    val arrayOfIngredients = arrayOf("")//вот тут
+
+
+    for (i in 0..numberOfIngredients) {
+        println("Введите ингредиент № ${i+1}")
+        arrayOfIngredients[i] = readln()
+        println("${arrayOfIngredients[i]}")
+    }
+}
+
+
