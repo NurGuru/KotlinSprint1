8e1370f0847051c99b63ae27e44e2d642d5c26c3
NurGuru <nurchibald@mail.ru>
2023-03-23 09:41:58 +0300

    KS-8-3

--- src/main/kotlin/lesson_8/lesson_8_task_3.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_3.kt
@@ -1,2 +1,13 @@
 package lesson_8
 
+fun main() {
+    val arrayOfIngredients = arrayOf("яйцо", "грибы", "сметана", "перец", "соль")
+    println("Какой ингредиент найти?")
+    val searchingIngredient = readln()
+
+    if (arrayOfIngredients.indexOf("$searchingIngredient") >= 0) {
+        println(("Ингредиент [$searchingIngredient] в рецепте есть"))
+    } else {
+        println("Такого ингредиента в рецепте нет.")
+    }
+}
