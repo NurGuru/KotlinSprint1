6d7fe8f9fc6a4bd6ba14215e26a2963cbc302ea5
NurGuru <nurchibald@mail.ru>
2023-03-26 10:28:41 +0300

    KS-7.5 Блин а как сделать, чтоб все символы из цикла записать в одну переменную?

--- src/main/kotlin/lesson_7/lesson_7_task_5.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_5.kt
@@ -3,14 +3,12 @@ package lesson_7
 fun main() {
     println("Сколько символов в пароле?")
     val passwordLength = readln().toInt()
-    val number = (1..9)
-    val letter = ('a'..'z')
-    val letterUp = ('A'..'Z')
+    val symbols = (1..9) + ('a'..'z') + ('A'..'Z')
 
-        for (i in passwordLength downTo 1) {
-        var symbol = ((number + letter + letterUp).random()).toString()
-        symbol+=symbol
-        println(symbol)
+    for (i in passwordLength downTo 1) {
+            var password = symbols.random().toString()
+            password+=password+symbols
+        println(password)
     }
 
 
