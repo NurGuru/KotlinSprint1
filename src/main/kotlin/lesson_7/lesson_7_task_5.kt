54a5cfdf0057fdd1cb7a33a7de91e3bb8960d5ac
NurGuru <nurchibald@mail.ru>
2023-03-26 17:09:50 +0300

    KS-7.5 Доделал

--- src/main/kotlin/lesson_7/lesson_7_task_5.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_5.kt
@@ -4,12 +4,12 @@ fun main() {
     println("Сколько символов в пароле?")
     val passwordLength = readln().toInt()
     val symbols = (1..9) + ('a'..'z') + ('A'..'Z')
+    var password = ""
 
     for (i in passwordLength downTo 1) {
-            var password = symbols.random().toString()
-            password+=password+symbols
-        println(password)
+            password+=symbols.random()
     }
+    println(password)
 
 
 }
