bd51ebe69704aa17b4555bb278c476f74754b5a6
NurGuru <nurchibald@mail.ru>
2023-03-27 09:11:00 +0300

    KS-9-5

--- src/main/kotlin/lesson_9/lesson_9_task_5.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_5.kt
@@ -1,2 +1,12 @@
 package lesson_9
 
+fun main() {
+    println("Введите 5 ингредиентов")
+    var ingredients = mutableListOf(readln(), readln(), readln(), readln(), readln())
+
+
+    ingredients.add(".")
+    ingredients.map {ingredients[2].uppercase() }
+    println(ingredients)
+
+}
