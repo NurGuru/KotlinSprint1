ade48cb8278c3b33ad8c7cea37bfe72115e6910d
NurGuru <nurchibald@mail.ru>
2023-03-27 07:25:52 +0300

    KS-9-1

--- src/main/kotlin/lesson_9/lesson_9_task_1.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_1.kt
@@ -1,10 +1,2 @@
 package lesson_9
 
-fun main() {
-    val ingredients = listOf("яйца","сметана", "соль", "перец")
-
-    println("В рецепте есть следующие ингредиенты $ingredients")
-    ingredients.forEach { (println(it))
-    }
-
-}
