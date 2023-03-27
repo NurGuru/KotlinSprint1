ee53f6b15fc5b842233a5522d0d4a40eee4a36f3
NurGuru <nurchibald@mail.ru>
2023-03-27 10:06:11 +0300

    KS-10-2

--- src/main/kotlin/lesson_10/lesson_10_task_2.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_2.kt
@@ -1,2 +1,16 @@
 package lesson_10
+fun main() {
+    validation()
+    }
 
+fun validation (){
+    println("Введите логин")
+    val login = readln()
+    println("Введите пароль")
+    val password = readln()
+    if (password.length<4){
+        println("Логин или пароль недостаточно длинные")
+    }else{
+        println("Добро пожаловать")
+    }
+}
