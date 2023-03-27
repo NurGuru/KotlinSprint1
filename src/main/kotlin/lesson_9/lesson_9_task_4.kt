648023b7cbf8520210ec61b363fdabf37de9d1d9
NurGuru <nurchibald@mail.ru>
2023-03-27 08:37:28 +0300

    KS-9-4 зашел в тупик

--- src/main/kotlin/lesson_9/lesson_9_task_4.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -1,2 +1,8 @@
 package lesson_9
+    fun main(){
+        println("Введите 5 ингредиентов ")
+        var ingredients = mutableListOf("${readln()},${readln()},${readln()},${readln()},${readln()}")
+        println(ingredients)
+        println(ingredients.sort())
 
+    }
