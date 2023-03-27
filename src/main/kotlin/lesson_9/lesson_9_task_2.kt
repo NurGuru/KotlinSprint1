f804ebc366a4a3c0ca20f1747a3b50722898699f
NurGuru <nurchibald@mail.ru>
2023-03-27 15:07:50 +0300

    KS-9-2 исправил

--- src/main/kotlin/lesson_9/lesson_9_task_2.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_2.kt
@@ -1,2 +1,18 @@
 package lesson_9
 
+fun main() {
+    val ingredients = mutableListOf("яйцо", "сметана", "хлеб")
+    ingredients.forEach { println(it) }
+
+    println("В рецепте есть базоывае ингредиенты: $ingredients")
+    println("Желаете добавить еще?")
+    val askToAdd = readln()
+
+    if (askToAdd == "Да" || askToAdd == "да") {
+        println("Какой ингредиент вы хотите добавить")
+        val addIngredient = readln()
+        ingredients.add("$addIngredient")
+        println("Тепреь в рецепте следующие ингредиенты:$ingredients")
+    }
+
+}
