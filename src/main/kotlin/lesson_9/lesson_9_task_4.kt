c392db01ac4de60b1484d713ef0f654ec0f02ff0
NurGuru <nurchibald@mail.ru>
2023-03-28 10:23:35 +0300

    KS-9-4 доделал

--- src/main/kotlin/lesson_9/lesson_9_task_4.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -1,8 +1,7 @@
 package lesson_9
     fun main(){
         println("Введите 5 ингредиентов ")
-        var ingredients = mutableListOf("${readln()},${readln()},${readln()},${readln()},${readln()}")
-        println(ingredients)
-        println(ingredients.sort())
+        val ingredients =readln()
 
+        println(ingredients.split(",").sorted())
     }
