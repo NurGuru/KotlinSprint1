de6e32fead8d0f51b590d9a1cc562c94693002df
NurGuru <nurchibald@mail.ru>
2023-03-29 00:51:56 +0300

    KS-10-5 исправил

--- src/main/kotlin/lesson_10/lesson_10_task_5.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_5.kt
@@ -10,38 +10,33 @@ fun main() {
     val password = createPassword()
 
     println("Авторизация")
-    var programLogin = ""
-    var programPassword = ""
+
     do {
         println("Введите логин")
-        programLogin = readln()
+        val programLogin = readln()
         println("Введите пароль")
-        programPassword = readln()
+        val programPassword = readln()
         if (programPassword != password || programLogin != login) {
             println("НЕВЕРНЫЕ ДАННЫЕ!!!\nПопробуйте еще раз.")
         }
     } while (programPassword != password || programLogin != login)
     println()
 
-
-do {
-    val code = createCode()
-    println("Введите код из смс")
-    val programCode = readln().toInt()
-    if (code!=programCode){
-        println("НЕВЕРНЫЙ КОД!!!\nМы вышлем новый код, попробуйте еще раз")
-        println()
-    }
-}while (code!=programCode)
+    do {
+        val code = createCode()
+        println("Введите код из смс")
+        val programCode = readln().toInt()
+        if (code != programCode) {
+            println("НЕВЕРНЫЙ КОД!!!\nМы вышлем новый код, попробуйте еще раз\n")
+        }
+    } while (code != programCode)
 
     println("Вы авторизовались!!")
-
 }
 
 fun validation(loginLength: Int): Boolean {
-    if (loginLength < 4) {
+    if (loginLength < 4)
         println("Логин недостаточно длинный")
-    }
     return loginLength < 4
 }
 
@@ -51,15 +46,12 @@ fun createPassword(): String {
     for (i in 1..9) {
         password += symbols.random()
     }
-    println("Ваш пароль:$password")
-    println()
+    println("Ваш пароль:$password\n")
     return password
 }
-
 fun createCode(): Int {
     val code = (1000..9999).random()
 
-    println("пришло смс:\nВаш код:$code")
-    println()
+    println("пришло смс:\nВаш код:$code\n")
     return code
 }
