e6de243c3c6144f8b8db31202f9935c85bb62b6c
NurGuru <nurchibald@mail.ru>
2023-03-28 16:22:26 +0300

    KS-9-5 доделал

--- src/main/kotlin/lesson_9/lesson_9_task_5.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_5.kt
@@ -4,18 +4,21 @@ fun main() {
     println("Введите 5 ингредиентов")
     var ingredients = mutableSetOf(readln())
 
-    for (i in 0..3){
+    for (i in 0..3) {
         ingredients.add("${readln()}")
     }
 
-    ingredients=ingredients.sorted().toMutableSet()
-    ingredients.add(".")
-
+    ingredients = ingredients.sorted().toMutableSet()
     println(ingredients)
 
+    val new = ingredients.toMutableList()
+    new[0] = new[0][0].uppercase() + new[0].substring(1)
+    new[new.lastIndex] = new[new.lastIndex] + "."
 
-//    ingredients.toMutableList()[0].capitalize()
-//    ingredients.first().uppercase()
-//    println( )
-//    ingredients.add(".")
+    println(new)
 }
+
+
+
+
+
