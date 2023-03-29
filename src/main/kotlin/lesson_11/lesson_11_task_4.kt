c10f525782d0a81d66f9153552fd535fec97cedb
NurGuru <nurchibald@mail.ru>
2023-03-29 23:47:25 +0300

    KS-11-4 сделал как понял

--- src/main/kotlin/lesson_11/lesson_11_task_4.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_4.kt
@@ -1,2 +1,38 @@
 package lesson_11
 
+class Contact(
+    val firstName: String,
+    val secondName: String,
+    val mobilePhone: Long, homePhone: Long,
+    val mail: String,
+    val favoriteContacts: List<String>,
+) {
+    fun writeMessage() {
+        println("Написать сообщение")
+        val message = readln()
+    }
+
+    fun makeCall(firstName: String, secondName: String) {
+        println("Вы звоните $firstName $secondName")
+    }
+
+    fun makeVideoCall(firstName: String, secondName: String) {
+        println("Вы звоните по видео $firstName $secondName")
+    }
+
+    fun writeOnMail(mail: String) {
+        println("Написать на почту $mail")
+    }
+
+}
+
+fun main() {
+    val contact1 = Contact(
+        "First Name",
+        "Second Name",
+        89999994242,
+        89998884242,
+        "mail@mail.ru",
+        favoriteContacts = listOf("жена", "подруга", "подруга")
+    )
+}
