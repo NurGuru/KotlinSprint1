c50ac0393b58ddf0463a54b61cefb49ed889a888
NurGuru <nurchibald@mail.ru>
2023-03-29 12:01:29 +0300

    KS-11-1

--- src/main/kotlin/lesson_11/lesson_11_task_1.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_1.kt
@@ -1,2 +1,11 @@
 package lesson_11
 
+class User(val id: Int, val login: String, val password: String, val mail: String)
+
+fun main() {
+    val userOleg = User(23, "MrOleg", "oleg0707", "oleg@mail.ru")
+    val userSasha = User(27, "MrSasha", "Sasha0505", "sasha@mail.ru")
+
+    println("Данные Олега: ${userOleg.id}, ${userOleg.login}, ${userOleg.password}, ${userOleg.mail}\n")
+    println("Данные Саши: ${userSasha.id}, ${userSasha.login}, ${userSasha.password}, ${userSasha.mail}")
+}
