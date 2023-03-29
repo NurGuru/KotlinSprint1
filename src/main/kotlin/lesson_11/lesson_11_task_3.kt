e8ad7170ea0cf474a339480a9d12bceeb7620816
NurGuru <nurchibald@mail.ru>
2023-03-30 00:13:36 +0300

    KS-11-3 помоги))

--- src/main/kotlin/lesson_11/lesson_11_task_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -1,2 +1,16 @@
 package lesson_11
 
+//class Room(val img: String, val name: String, val avatars: List<String>, val avatarStatus:List<String>) {
+//
+//    fun showAvatar(avatar: List<String>, namber: Int, nameOfMember:String) {
+//        println("выберите участника")
+//        namber= readln().toInt()
+//
+//    }
+//
+//}
+//
+//fun main() {
+
+
+//}
