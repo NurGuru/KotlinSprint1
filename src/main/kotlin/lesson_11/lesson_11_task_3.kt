99343760097d08d584b49d41d26660bbc84e246c
NurGuru <nurchibald@mail.ru>
2023-03-30 13:22:39 +0300

    KS-11-3

--- src/main/kotlin/lesson_11/lesson_11_task_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -1,16 +1,24 @@
 package lesson_11
 
-//class Room(val img: String, val name: String, val avatars: List<String>, val avatarStatus:List<String>) {
-//
-//    fun showAvatar(avatar: List<String>, namber: Int, nameOfMember:String) {
-//        println("выберите участника")
-//        namber= readln().toInt()
-//
-//    }
-//
-//}
-//
-//fun main() {
+class RoomCard(val img: String, val roomName: String, val members: List<User>) {}
 
+class User(val name: String, val avatar: String) {
+}
 
-//}
+fun main() {
+    val user1 = User("John", "картинка аватара пользователя")
+    val user2 = User("Steve", "картинка аватара пользователя")
+    val room1 = RoomCard("картинка автара комнаты", "English Speaking Club", listOf(user1, user2))
+    println(room1.img)
+    println(room1.roomName)
+    println(user1.name)
+    println(user2.name)
+
+    val user3 = User("Oleg", "картинка аватара пользователя")
+    val user4 = User("Sasha", "картинка аватара пользователя")
+    val room2 = RoomCard("картинка автара комнаты", "Russian Speaking Club", listOf(user3, user4))
+    println(room2.img)
+    println(room2.roomName)
+    println(user3.name)
+    println(user4.name)
+}
