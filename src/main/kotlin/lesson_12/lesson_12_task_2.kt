1205845864c541a08bbe2db5646600a80ccfba87
NurGuru <nurchibald@mail.ru>
2023-03-31 16:44:45 +0300

    KS-12-2

--- src/main/kotlin/lesson_12/lesson_12_task_2.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_2.kt
@@ -1,2 +1,27 @@
 package lesson_12
 
+class WeatherChange2(_dayTemperature: Int, _nightTemperature: Int, _rain: Boolean, _atmospherePressure: Int) {
+    var dayTemperature = _dayTemperature
+    var nightTemperature = _nightTemperature
+    var rain = _rain
+    var atmospherePressure = _atmospherePressure
+
+    fun printWeather() {
+        println(dayTemperature)
+        println(nightTemperature)
+        println(rain)
+        println(atmospherePressure)
+    }
+}
+
+fun main() {
+    val weatherYesterday = WeatherChange2(26, 12, true, 200)
+    weatherYesterday.printWeather()
+    println()
+    val weatherToday = WeatherChange2(35, 20, false, 400)
+    weatherToday.printWeather()
+    println()
+    val weatherUsually = WeatherChange2(24, 11, false, 300)
+    weatherUsually.printWeather()
+
+}
