9013243b36e3e916336a6bf1b80c83258fc7d3db
NurGuru <nurchibald@mail.ru>
2023-03-31 16:19:16 +0300

    KS-12-1

---
+++ src/main/kotlin/lesson_12/lesson_12_task_1.kt
@@ -0,0 +1,27 @@
+package lesson_12
+
+class WeatherChange() {
+    var dayTemperature = 26
+    var nightTemperature = 11
+    var rain = true
+    var atmospherePressure = 123
+
+    fun printWeather() {
+        println(dayTemperature)
+        println(nightTemperature)
+        println(rain)
+        println(atmospherePressure)
+    }
+}
+
+fun main() {
+    val weatherYesterday = WeatherChange()
+    weatherYesterday.printWeather()
+    println()
+    val weatherToday = WeatherChange()
+    weatherToday.dayTemperature = 34
+    weatherToday.nightTemperature = 20
+    weatherToday.rain = false
+    weatherToday.atmospherePressure = 200
+    weatherToday.printWeather()
+}
