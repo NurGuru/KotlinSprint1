efe59b65a8b57a0d0e5f352db2c5dbcfce15a480
NurGuru <nurchibald@mail.ru>
2023-03-31 13:14:18 +0300

    KS-11-3 опять недопонял

--- src/main/kotlin/lesson_11/lesson_11_task_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -1,24 +1,25 @@
 package lesson_11
 
-class RoomCard(val img: String, val roomName: String, val members: List<User>) {}
+import kotlin.String
+import kotlin.String as String1
 
-class User(val name: String, val avatar: String) {
+class RoomCard(val img: String1, val roomName: String1, val members: List<User>) {}
+
+class User(
+    val name: String1,
+    val avatar: String1,
+    val status: String = "разговаривает,микрофон выключен,пользователь заглушен,"
+) {
 }
 
 fun main() {
     val user1 = User("John", "картинка аватара пользователя")
     val user2 = User("Steve", "картинка аватара пользователя")
     val room1 = RoomCard("картинка автара комнаты", "English Speaking Club", listOf(user1, user2))
-    println(room1.img)
-    println(room1.roomName)
-    println(user1.name)
-    println(user2.name)
+
 
     val user3 = User("Oleg", "картинка аватара пользователя")
     val user4 = User("Sasha", "картинка аватара пользователя")
     val room2 = RoomCard("картинка автара комнаты", "Russian Speaking Club", listOf(user3, user4))
-    println(room2.img)
-    println(room2.roomName)
-    println(user3.name)
-    println(user4.name)
+
 }
