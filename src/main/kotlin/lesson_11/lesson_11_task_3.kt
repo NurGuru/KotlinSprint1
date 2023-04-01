99d4de494be966cdbd81588744c4355efcfa16f6
NurGuru <nurchibald@mail.ru>
2023-04-01 10:58:59 +0300

    KS-11-3 доделал

--- src/main/kotlin/lesson_11/lesson_11_task_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -1,25 +1,47 @@
 package lesson_11
 
-import kotlin.String
-import kotlin.String as String1
+enum class UserStatus(val label: String) {
+    TALK("разговаривает"),
+    MIC_OFF(".."),
+    USER_MUTED("..");
+}
 
-class RoomCard(val img: String1, val roomName: String1, val members: List<User>) {}
+class RoomCard(
+    val img: String,
+    val roomName: String,
+    val members: List<User>
+)
 
 class User(
-    val name: String1,
-    val avatar: String1,
-    val status: String = "разговаривает,микрофон выключен,пользователь заглушен,"
-) {
-}
+    val name: String,
+    val avatar: String,
+    val status: UserStatus,
+)
 
 fun main() {
-    val user1 = User("John", "картинка аватара пользователя")
-    val user2 = User("Steve", "картинка аватара пользователя")
-    val room1 = RoomCard("картинка автара комнаты", "English Speaking Club", listOf(user1, user2))
+    val user1 = User("John", "картинка аватара пользователя", UserStatus.USER_MUTED)
+    val user2 = User("Steve", "картинка аватара пользователя", UserStatus.TALK)
+    val user3 = User("Oleg", "картинка аватара пользователя", UserStatus.MIC_OFF)
+    val user4 = User("Sasha", "картинка аватара пользователя", UserStatus.TALK)
 
+    val rooms = listOf(
+        RoomCard(
+            img = "картинка автара комнаты",
+            roomName = "Russian Speaking Club",
+            members = listOf(user3, user4)
+        ),
+        RoomCard(
+            img = "картинка автара комнаты",
+            roomName = "English Speaking Club",
+            members = listOf(user1, user2)
+        )
+    )
+    rooms.forEach {
+        println("${it.roomName},${it.img},${it.members}")
+    }
 
-    val user3 = User("Oleg", "картинка аватара пользователя")
-    val user4 = User("Sasha", "картинка аватара пользователя")
-    val room2 = RoomCard("картинка автара комнаты", "Russian Speaking Club", listOf(user3, user4))
 
 }
+
+
+
