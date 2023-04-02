a2de968ce18da1a516014ab9761a9e9d62d190e6
NurGuru <nurchibald@mail.ru>
2023-04-03 00:50:37 +0300

    KS-13-3

--- src/main/kotlin/lesson_13/lesson_13_task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -1,2 +1,33 @@
 package lesson_13
 
+
+class TelephoneBook2(
+    val name: String,
+    val phone: Long,
+    var company: String?
+) {
+    fun printInfo() {
+        val _name = name
+        val _phone = phone
+        val _company = company ?: "[не указано]"
+        println("Имя:$_name\nНомер:$_phone\nКомпания:$_company\n")
+    }
+}
+
+fun main() {
+    val contact1 = TelephoneBook2("Ростислав", 89123456789, null)
+    val contact2 = TelephoneBook2("Ростислав", 89123456789, "Tg")
+    val contact3 = TelephoneBook2("Ростислав", 89123456789, "VK")
+    val list: MutableList<TelephoneBook2> = mutableListOf()
+
+    list.add(contact1)
+    list.add(contact2)
+    list.add(contact3)
+
+    for (i in list) i.printInfo()
+}
+
+
+
+
+
