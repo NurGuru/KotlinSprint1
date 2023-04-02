8c9eca90df2e5abe920383f7f05cd48fbefbba36
NurGuru <nurchibald@mail.ru>
2023-04-03 00:16:03 +0300

    KS-13-2

--- src/main/kotlin/lesson_13/lesson_13_task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -1,2 +1,19 @@
 package lesson_13
 
+class TelephoneBook1(val name: String, val phone: Long, var company: String? = null) {
+
+    fun printInfo(name: String, phone: Long, company: String?) {
+
+        println("Имя:$name\nНомер:$phone\nКомпания:$company")
+    }
+}
+
+fun main() {
+    val contact = TelephoneBook1("Ростислав", 89123456789, null)
+
+    if (contact.company == null) {
+        contact.company = "[не указано]"
+    }
+
+    contact.printInfo(contact.name, contact.phone, contact.company)
+}
