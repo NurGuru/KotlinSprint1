f74997469e305e4f7e51ce4e296fd1429f5c9c23
NurGuru <nurchibald@mail.ru>
2023-04-04 02:04:43 +0300

    KS-14-1

--- src/main/kotlin/lesson_14/lesson_14_task_1.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_1.kt
@@ -1,2 +1,28 @@
 package lesson_14
 
+class StandartShip(
+    val name: String,
+    val type: String,
+    val speed: Int,
+    val loadAbility: Int,
+    val aceBrake: Boolean = false
+) {
+    fun printShipSpeed() {
+        println("Скорость корабля $name: $speed")
+    }
+
+    fun printSheepLoadAbility() {
+        println("Вмсетительность корабля $name: $loadAbility ящиков")
+    }
+}
+
+fun main() {
+    val standartShip=StandartShip(
+        "Корабль",
+        "Стандартный  корабль",
+        50,
+        500
+    )
+    standartShip.printShipSpeed()
+    standartShip.printSheepLoadAbility()
+}
