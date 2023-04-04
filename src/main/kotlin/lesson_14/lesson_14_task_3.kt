342c5741c781d23e2b55855350c9927a6a7b11bd
NurGuru <nurchibald@mail.ru>
2023-04-05 00:26:01 +0300

    KS-14-3

--- src/main/kotlin/lesson_14/lesson_14_task_3.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_3.kt
@@ -1,2 +1,24 @@
 package lesson_14
 
+open class Message(
+    val authorName: String,
+    val postText: String
+) {
+    fun makePost() {
+        println("Пост пользователя $authorName:\n$postText")
+    }
+}
+
+class Comments(val authorOfComment: String, val commentText: String) : Message(authorOfComment, commentText) {
+    fun leaveComment(whosePostCommenting: String, whichPostCommenting: String) {
+        println("$authorOfComment: $commentText\nкоментарий к посту:\n$whosePostCommenting: $whichPostCommenting")
+    }
+}
+
+fun main() {
+    val newPost = Message("John", "I love playing football!")
+    newPost.makePost()
+    println()
+    val newComment = Comments("Jimmy", "I love football too!")
+    newComment.leaveComment(newPost.authorName, newPost.postText)
+}
