bc78960dd3884f8344f8faba7023f6a3f1aad864
NurGuru <nurchibald@mail.ru>
2023-04-07 00:56:21 +0300

    KS-15-5

--- src/main/kotlin/lesson_15/lesson_15_task_5.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_5.kt
@@ -1,2 +1,43 @@
 package lesson_15
 
+interface Movable {
+    val carType: String
+    fun move() {
+        println("$carType начал движение")
+    }
+}
+
+interface PassengerTransportable {
+    val passengerTransportingCapacity: Byte
+    fun passengerTransporting(carType: String) {
+        println("$carType перевозит $passengerTransportingCapacity пассажиров")
+    }
+}
+
+interface CargoTransportable {
+    val cargoTransportingCapacity: Byte
+    fun cargoTransporting(carType: String, passengerQuantity: Byte) {
+        println("$carType перевозит $cargoTransportingCapacity тонны груза и $passengerQuantity пассажира")
+    }
+}
+
+class CargoCar(
+    override val carType: String,
+    override val passengerTransportingCapacity: Byte = 1,
+    override val cargoTransportingCapacity: Byte = 2
+) : Movable, PassengerTransportable, CargoTransportable
+
+class CommonCar(
+    override val carType: String,
+    override val passengerTransportingCapacity: Byte = 3
+) : Movable, PassengerTransportable
+
+fun main() {
+    val car1 = CommonCar("Легковушка")
+    car1.move()
+    car1.passengerTransporting(car1.carType)
+    println()
+    val cargo1 = CargoCar("Грузовик")
+    cargo1.move()
+    cargo1.cargoTransporting(cargo1.carType, cargo1.passengerTransportingCapacity)
+}
