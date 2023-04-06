9cdb549bc9d2c1b41c3851c4fca5573e54d356ec
NurGuru <nurchibald@mail.ru>
2023-04-06 23:12:46 +0300

    KS-15-1

--- src/main/kotlin/lesson_15/lesson_15_task_1.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_1.kt
@@ -1,2 +1,42 @@
 package lesson_15
 
+interface Fly {
+    fun flying() {
+        println("Это существо летает")
+    }
+}
+
+interface Swim {
+    fun swimming() {
+        println("Это существо плавает")
+    }
+}
+
+class Karas() : Swim {
+    override fun swimming() {
+        println("Карась плавающее существо")
+    }
+}
+
+class Seagull() : Fly {
+    override fun flying() {
+        println("Чайка летающее существо")
+    }
+}
+
+class Duck() : Fly {
+    override fun flying() {
+        println("Утка летающее существо")
+    }
+}
+
+fun main() {
+    val karas = Karas()
+    karas.swimming()
+
+    val seagull = Seagull()
+    seagull.flying()
+
+    val duck = Duck()
+    duck.flying()
+}
