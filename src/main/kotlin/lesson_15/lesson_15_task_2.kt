e567a655950f6046208e52cf88b88fdc670c4146
NurGuru <nurchibald@mail.ru>
2023-04-06 23:26:39 +0300

    KS-15-2

--- src/main/kotlin/lesson_15/lesson_15_task_2.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_2.kt
@@ -1,2 +1,24 @@
 package lesson_15
 
+abstract class Message(val text: String) {
+    fun connectToServer() {
+        println("Соединение с сервером...")
+    }
+
+    fun sendText() {
+        println("Отправка сообщения:\n$text")
+    }
+}
+
+class Temperature(text: String) : Message(text)
+class Precipitation(text: String) : Message(text)
+
+fun main() {
+    val sendTemperature = Temperature("Температура: -8")
+    sendTemperature.connectToServer()
+    sendTemperature.sendText()
+    println()
+    val sendPrecipitation = Precipitation("Выпало много снега")
+    sendPrecipitation.connectToServer()
+    sendPrecipitation.sendText()
+}
