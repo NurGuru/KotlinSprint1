9b69c13158f0750196efdb0ced6c274edcffda86
NurGuru <nurchibald@mail.ru>
2023-04-07 00:07:31 +0300

    KS-15-4

--- src/main/kotlin/lesson_15/lesson_15_task_4.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_4.kt
@@ -1,2 +1,32 @@
 package lesson_15
 
+abstract class Product(
+    val name: String,
+    val quantity: Int
+)
+
+interface Search {
+    fun searchProduct(searchingProduct: String) {
+        println("Выполняется поиск товара: $searchingProduct")
+    }
+}
+
+class Instruments(
+    name: String,
+    quantity: Int
+) : Product(name, quantity), Search
+
+class ComplectStuff(
+    name: String,
+    quantity: Int
+) : Product(name, quantity)
+
+fun main() {
+    val piano = Instruments("Пианино", 4)
+    piano.searchProduct("Пианино")
+    println(piano.quantity)
+
+    val pianoKey = ComplectStuff("Клавиши пианино", 148)
+    println("Комплектующего товара: ${pianoKey.name} осталось ${pianoKey.quantity} ")
+
+}
