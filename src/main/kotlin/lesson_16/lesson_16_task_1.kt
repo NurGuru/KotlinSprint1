bb2a0d3b72fd616702e80ea93280d00dcc597f86
NurGuru <nurchibald@mail.ru>
2023-04-07 23:51:11 +0300

    KS-16-1

--- src/main/kotlin/lesson_16/lesson_16_task_1.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_1.kt
@@ -1,2 +1,13 @@
 package lesson_16
 
+class GameStone(private val number: Int = (1..6).random()) {
+
+    fun showNumber() {
+        println("Выпало число $number")
+    }
+}
+
+fun main() {
+    val stone = GameStone()
+    stone.showNumber()
+}
