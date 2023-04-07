07fcc2193211322aded53a23bc4a1ec46687f7d5
NurGuru <nurchibald@mail.ru>
2023-04-08 00:15:18 +0300

    KS-16-2

--- src/main/kotlin/lesson_16/lesson_16_task_2.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_2.kt
@@ -1,2 +1,23 @@
 package lesson_16
 
+import kotlin.math.pow
+
+class Circle(protected val radius: Double) {
+    private val pi: Double = 3.14
+
+    fun getCirclePerimeter(): Double {
+        return 2 * radius * pi
+    }
+
+    fun getCircleSquare(): Double {
+        return pi * radius.pow(2.0)
+    }
+}
+
+fun main() {
+    val circlePerimeter = Circle(6.5)
+    println("Периметр круга = ${circlePerimeter.getCirclePerimeter()}")
+
+    val circleSquare = Circle(6.5)
+    println("Площадь круга = ${circleSquare.getCircleSquare()}")
+}
