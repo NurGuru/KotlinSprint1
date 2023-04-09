6f3fd2bc7157497aa0e0cbe797243e3110ef177b
NurGuru <nurchibald@mail.ru>
2023-04-09 16:36:36 +0300

    KS-11-5

--- src/main/kotlin/lesson_11/lesson_11_task_5.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_5.kt
@@ -1,18 +1,34 @@
 package lesson_11
 
-interface Forum {
-    fun createNewUser(id: Int, login: String, password: String, mail: String): User2 {
-        return User2(id, login, password, mail)
+class Forum() {
+    fun createNewUser(id: Int, login: String, password: String, mail: String): User1 {
+        return User1(id, login, password, mail)
     }
 
-    fun newMessage(authorId: Int, userName: String, message: String) {
-
-
+    fun newMessage(authorId: Int, authorName: String, message: String): String {
+        return message
     }
 
-    fun printTread(){
-        println(createNewUser)
+    fun printTread(author: String, message: String) {
+        println("$author: $message")
+        Thread.sleep(1000)
     }
-
-
 }
+
+fun main() {
+    val creator = Forum()
+
+    val newUser1 = creator.createNewUser(1, "John", "0707", "john@mail.ru")
+    val message1FromNewUser1 = creator.newMessage(newUser1.id, newUser1.login, "Hi Jack!")
+    val message2FromNewUser1 = creator.newMessage(newUser1.id, newUser1.login, "How are you?")
+
+    val newUser2 = creator.createNewUser(2, "Jack", "0505", "jack@mail.ru")
+    val message1FromNewUser2 = creator.newMessage(newUser2.id, newUser2.login, "Hi John!")
+    val message2FromNewUser2 = creator.newMessage(newUser2.id, newUser2.login, "I am fine!")
+
+    creator.printTread(newUser1.login, message1FromNewUser1)
+    creator.printTread(newUser1.login, message2FromNewUser1)
+    creator.printTread(newUser2.login, message1FromNewUser2)
+    creator.printTread(newUser2.login, message2FromNewUser2)
+}
+
