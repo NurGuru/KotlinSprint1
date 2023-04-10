8ac20c2c0647810ddccf243bd13043eca9552470
NurGuru <nurchibald@mail.ru>
2023-04-10 16:20:51 +0300

    KS-16-5

--- src/main/kotlin/lesson_16/lesson_16_task_5.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_5.kt
@@ -1,2 +1,41 @@
 package lesson_16
 
+class Player(
+    val name: String,
+    private var health: Int,
+    var punchStrength: Int,
+) {
+    var playerAlive = true
+
+    fun takeDamage(takingDamageStrength: Int) {
+        println("Нанесено $takingDamageStrength урона здоровью ")
+        health -= takingDamageStrength
+        if (health > 0) println("Ваше здоровье = $health")
+        else death()
+    }
+
+    private fun death() {
+        playerAlive = false
+        punchStrength = 0
+        health = 0
+        println("Потрачено!")
+    }
+
+    fun healingSlave(heal: Int) {
+        if (playerAlive) {
+            println("Востановлено $heal очков здоровья")
+            health += heal
+            println("Ваше здоровье = $health")
+        } else {
+            println("Вы мертвы и не можете лечиться(")
+        }
+    }
+}
+
+fun main() {
+    val player1 = Player("John", 100, 20)
+    player1.takeDamage(90)
+    player1.healingSlave(30)
+    player1.takeDamage(60)
+    player1.healingSlave(10)
+}
