6dd3771657864b208978f4af99778062388c45ff
NurGuru <nurchibald@mail.ru>
2023-04-11 00:22:32 +0300

    KS-11-5 реализовал добавление id через класс форум, создал список сообщений

--- src/main/kotlin/lesson_11/lesson_11_task_5.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_5.kt
@@ -1,34 +1,60 @@
 package lesson_11
 
+
 class Forum() {
-    fun createNewUser(id: Int, login: String, password: String, mail: String): User1 {
-        return User1(id, login, password, mail)
+    var mainId: Int=0
+    var messages: MutableList<NewMessage> = mutableListOf()
+
+    fun createNewUser(login: String, password: String, mail: String): User5 {
+        mainId+=+1
+        return User5(mainId, login, password, mail)
+
     }
 
-    fun newMessage(authorId: Int, authorName: String, message: String): String {
-        return message
+    fun newMessage(authorId: Int, authorName: String, message: String): NewMessage {
+
+        return NewMessage(authorId, authorName, message)
     }
 
-    fun printTread(author: String, message: String) {
-        println("$author: $message")
-        Thread.sleep(1000)
+    fun printTread() {
+
+        for (i in messages) {
+            var messageAuthorName= messages[1]
+            var messageText= messages[2]
+            println("$messageAuthorName: $messageText")
+            Thread.sleep(1000)
+        }
+//
     }
 }
+
+class User5(val id: Int, val login: String, val password: String, val mail: String)
+class NewMessage(authorId: Int, authorName: String, message: String)
 
 fun main() {
     val creator = Forum()
 
-    val newUser1 = creator.createNewUser(1, "John", "0707", "john@mail.ru")
+    val newUser1 = creator.createNewUser("John", "0707", "john@mail.ru")
     val message1FromNewUser1 = creator.newMessage(newUser1.id, newUser1.login, "Hi Jack!")
     val message2FromNewUser1 = creator.newMessage(newUser1.id, newUser1.login, "How are you?")
 
-    val newUser2 = creator.createNewUser(2, "Jack", "0505", "jack@mail.ru")
+
+    val newUser2 = creator.createNewUser("Jack", "0505", "jack@mail.ru")
     val message1FromNewUser2 = creator.newMessage(newUser2.id, newUser2.login, "Hi John!")
     val message2FromNewUser2 = creator.newMessage(newUser2.id, newUser2.login, "I am fine!")
 
-    creator.printTread(newUser1.login, message1FromNewUser1)
-    creator.printTread(newUser1.login, message2FromNewUser1)
-    creator.printTread(newUser2.login, message1FromNewUser2)
-    creator.printTread(newUser2.login, message2FromNewUser2)
+    creator.messages.addAll(
+        listOf(
+            message2FromNewUser1,
+            message2FromNewUser1,
+            message1FromNewUser2,
+            message2FromNewUser2
+        )
+    )
+
+
+
+    creator.printTread()
+
 }
 
