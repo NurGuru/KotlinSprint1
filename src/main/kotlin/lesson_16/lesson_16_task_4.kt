433e22fa3ca26c41f08cb010e6e3203665eb6689
NurGuru <nurchibald@mail.ru>
2023-04-10 15:33:59 +0300

    KS-16-4

--- src/main/kotlin/lesson_16/lesson_16_task_4.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_4.kt
@@ -1,2 +1,24 @@
 package lesson_16
 
+class Order(
+    val number: Int,
+    private var orderStatus: String
+) {
+    private var orderStatusChanging = false
+    fun sendRequest() {
+        println("Заявка отправлена")
+        orderStatusChanging = true
+    }
+
+    fun changeStatus() {
+        if (orderStatusChanging) orderStatus = "Заказ готов"
+        println(orderStatus)
+    }
+}
+
+fun main() {
+    val order = Order(47, "Заказ не готов")
+    order.sendRequest()
+    order.changeStatus()
+
+}
