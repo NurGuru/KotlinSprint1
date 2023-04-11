548f14ac783491fcd818010c442da0c50127c1b8
NurGuru <nurchibald@mail.ru>
2023-04-12 01:13:11 +0300

    KS-17-2

--- src/main/kotlin/lesson_17/lesson_17_task_2.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_2.kt
@@ -1,2 +1,24 @@
 package lesson_17
 
+class Ship(
+    val name: String,
+    val speed: Int,
+    val port: String
+) {
+    var shipName: String
+        get() = name
+        set(value) {
+            println("Нельзя менять имя корабля")
+        }
+}
+
+fun main() {
+    val ship = Ship(
+        "Титаник",
+        22,
+        "Порт 47"
+    )
+    println(ship.shipName)
+    ship.shipName = "Корсар"
+    println(ship.shipName)
+}
