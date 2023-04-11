c5c2e9a24d12df16cf132e005407af9e19b417fa
NurGuru <nurchibald@mail.ru>
2023-04-12 01:36:49 +0300

    KS-17-3

--- src/main/kotlin/lesson_17/lesson_17_task_3.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_3.kt
@@ -1,2 +1,20 @@
 package lesson_17
 
+class Package(
+    var name: String,
+    var failsNumber: Int,
+    val secret: Boolean
+) {
+    val packageName: String = name
+        get() = if (secret) "скрытая папка" else field
+
+    val packageFiles: Int = 0
+        get() = if (secret) 0 else field
+
+}
+
+fun main() {
+    val package1 = Package("Папка1", 27, true)
+    println(package1.packageName)
+    println(package1.packageFiles)
+}
