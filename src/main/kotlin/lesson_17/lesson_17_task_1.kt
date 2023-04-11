ca39563b5de35c5f071cef234615ef8c106bbcf9
NurGuru <nurchibald@mail.ru>
2023-04-12 00:38:46 +0300

    KS-17-1

--- src/main/kotlin/lesson_17/lesson_17_task_1.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_1.kt
@@ -1,2 +1,19 @@
 package lesson_17
 
+class Game {
+    var question: String = "Сколько тебе лет?"
+        set(value) {
+            field = value
+        }
+    var answer: String = ""
+        get() = "Мне 30 лет"
+        set(value) {
+            field = value
+        }
+}
+
+fun main() {
+    val game = Game()
+    println(game.question)
+    println(game.answer)
+}
