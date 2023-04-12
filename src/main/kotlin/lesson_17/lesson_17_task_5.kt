3d8d3202bd5d4bc301234595104bbf74e65d26bf
NurGuru <nurchibald@mail.ru>
2023-04-12 14:41:55 +0300

    KS-17-5

--- src/main/kotlin/lesson_17/lesson_17_task_5.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_5.kt
@@ -1,2 +1,29 @@
 package lesson_17
 
+
+class User17(
+    val login: String,
+    private val password: String
+) {
+
+    var userPassword = password
+        set(value) = println("Вы не можете поменять пароль!")
+//        get() = "****"
+//        field.length я как-то через длину пароля пытался, но не могу понять как мне потом  строку "*" умножить на длину
+
+    var userLogin = login
+        set(value) {
+            field = value
+            println("Логин успешно изменен!")
+        }
+}
+
+fun main() {
+    val user = User17("John", "0707")
+    user.userPassword = "00505"
+    println(user.userPassword)
+
+    user.userLogin = "Jack"
+    println(user.userLogin)
+}
+
