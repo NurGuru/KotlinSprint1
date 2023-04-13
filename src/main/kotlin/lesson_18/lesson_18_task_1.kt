65863421efa239617a8c51d512c88cba0dc6c9a4
NurGuru <nurchibald@mail.ru>
2023-04-13 20:10:58 +0300

    KS-18-1

--- src/main/kotlin/lesson_18/lesson_18_task_1.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_1.kt
@@ -1,2 +1,25 @@
 package lesson_18
 
+class Order() {
+    fun printOrder(
+        orderNumber: Int,
+        product: String
+    ) {
+        println("Заказан товар: $product")
+    }
+
+    fun printOrder(
+        orderNumber: Int,
+        product: List<String>
+    ) {
+        println("Заказаны следующие товары: $product")
+    }
+}
+
+fun main() {
+    val order1 = Order()
+    order1.printOrder(23, "Гиатара")
+
+    val order2 = Order()
+    order2.printOrder(24, listOf("Барабан", "Скрипка"))
+}
