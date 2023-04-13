fb560aaa7e2b8e552abab71a09b6fef0a1d12a32
NurGuru <nurchibald@mail.ru>
2023-04-13 21:10:46 +0300

    KS-18-4

--- src/main/kotlin/lesson_18/lesson_18_task_4.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_4.kt
@@ -1,2 +1,37 @@
 package lesson_18
 
+open class Box() {
+    open fun getBoxSquare(): Int {
+        return getBoxSquare()
+    }
+}
+
+class Rectangle(
+    val length: Int,
+    val width: Int,
+    val height: Int,
+) : Box() {
+    override fun getBoxSquare(): Int {
+        println("Площадь прямоугольной коробки")
+        return 2 * (length * width + length * width + width * height)
+    }
+}
+
+class Cube(
+    val length: Int,
+) : Box() {
+
+    override fun getBoxSquare(): Int {
+
+        println("Площадь кубической коробки")
+        return (length * length) * 6
+    }
+}
+
+fun main() {
+    val box1 = Rectangle(5, 6, 8)
+    println("${box1.getBoxSquare()}")
+
+    val box2 = Cube(5)
+    println("${box2.getBoxSquare()}")
+}
