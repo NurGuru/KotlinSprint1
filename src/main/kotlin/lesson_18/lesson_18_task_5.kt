86dae87865f0abe0594630edd44d13343d49b7ec
NurGuru <nurchibald@mail.ru>
2023-04-18 12:56:31 +0300

    KS-18-4

--- src/main/kotlin/lesson_18/lesson_18_task_5.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_5.kt
@@ -1,2 +1,38 @@
 package lesson_18
 
+
+class Point : Screen()
+class Square : Screen()
+class Circle : Screen()
+open class Screen {
+
+    fun draw(figure: Point, coordinateX: Float, coordinateY: Float) {
+        println("Создана ТОЧКА с центром: [$coordinateX, $coordinateY]")
+    }
+    fun draw(figure: Point, coordinateX: Int, coordinateY: Int) {
+        println("Создана ТОЧКА с центром: [$coordinateX, $coordinateY]")
+    }
+    fun draw(figure: Square, coordinateX: Int, coordinateY: Int) {
+        println("Создан КВАДРАТ с центром: [$coordinateX, $coordinateY]")
+    }
+    fun draw(figure: Square, coordinateX: Float, coordinateY: Float) {
+        println("Создан КВАДРАТ с центром: [$coordinateX, $coordinateY]")
+    }
+    fun draw(figure: Circle, coordinateX: Float, coordinateY: Float) {
+        println("Создан КРУГ с центром: [$coordinateX, $coordinateY]")
+    }
+    fun draw(figure: Circle, coordinateX: Int, coordinateY: Int) {
+        println("Создан КРУГ с центром: [$coordinateX, $coordinateY]")
+    }
+}
+fun main() {
+
+    val point = Screen()
+    point.draw(Point(), 0.6268F, 1.55797F)
+
+    val square = Screen()
+    square.draw(Square(), 2, 5)
+
+    val circle = Screen()
+    circle.draw(Circle(), 5F, 6.041F)
+}
