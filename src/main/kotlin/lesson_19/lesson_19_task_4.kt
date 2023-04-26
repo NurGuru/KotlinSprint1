98b206c01f5cd25a58c16b15276ab64fff520e0d
NurGuru <nurchibald@mail.ru>
2023-04-26 19:49:11 +0300

    KS-19-4

--- src/main/kotlin/lesson_19/lesson_19_task_4.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_4.kt
@@ -1,2 +1,38 @@
 package lesson_19
 
+enum class Patron {
+    BLUE,
+    GREEN,
+    RED,
+}
+
+class Tank {
+
+    private var currentPatron: Patron? = null
+
+    fun changePatron(patron: Patron) {
+
+        currentPatron = patron
+    }
+
+    fun shoot() {
+        when (currentPatron) {
+            Patron.BLUE -> println("Выстрел синими патроном (5 ед. урона)")
+            Patron.GREEN -> println("Выстрел зелеными патроном (10 ед. урона)")
+            Patron.RED -> println("Выстрел красными патроном (20 ед. урона)")
+            else -> println("Орудие не заряжено")
+        }
+    }
+}
+
+fun main() {
+    val tank = Tank()
+    tank.changePatron(Patron.RED)
+    tank.shoot()
+
+    tank.changePatron(Patron.GREEN)
+    tank.shoot()
+
+    tank.changePatron(Patron.BLUE)
+    tank.shoot()
+}
